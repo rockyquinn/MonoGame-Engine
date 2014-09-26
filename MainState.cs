ce525c09503a85b6cc6be9f2de39103ce1e817b3
Rocky Quinn <raq5889@rit.edu>
2014-09-26 15:47:36 -0400

    Create MainState.cs

---
+++ MainState.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoGame_Game_Engine
+{
+    class MainState
+    {
+        public static bool isCurrent;
+    }
+}
