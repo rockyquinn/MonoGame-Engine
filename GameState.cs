f64dd8c301c8c0d9a7fe7d29a571c0af37a93697
Rocky Quinn <raq5889@rit.edu>
2014-09-26 15:48:34 -0400

    Create GameState.cs

---
+++ GameState.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoGame_Game_Engine
+{
+    class GameState
+    {
+        public static bool isCurrent;
+    }
+}
