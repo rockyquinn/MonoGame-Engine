e4a1701a4d9faab664eb12a2d311fcf01d11eefd
Rocky Quinn <raq5889@rit.edu>
2014-09-26 15:46:59 -0400

    Created Map.cs

---
+++ Map.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame_Game_Engine
+{
+    class Map
+    {
+        private string mapList;
+        private string currentMap;
+
+        public void scroll(string direction)
+        {
+        }
+
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
